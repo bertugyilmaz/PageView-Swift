f46c8d848c9479eccb7450f4470885e8131a84d4
Bertuğ <ylmazbertug@gmail.com>
2017-05-31 11:14:04 +0300

    init project

---
+++ pageView/pageView/PageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageViewController.swift
+//  pageView
+//
+//  Created by Bertuğ YILMAZ on 31/05/2017.
+//  Copyright © 2017 bertug. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
---
+++ pageView/pageView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  pageView
+//
+//  Created by Bertuğ YILMAZ on 31/05/2017.
+//  Copyright © 2017 bertug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
